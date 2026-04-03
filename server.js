71f24499271b0deb3e427f682bc4e08056a67131
lukasz11jurek-crypto <lukasz11jurek@gmail.com>
2026-04-03 16:11:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import express from "express";
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/voice", (req, res) => {
-  res.set("Content-Type", "text/xml");
-
+  res.type("text/xml");
   res.send(`
     <Response>
-      <Say voice="Polly.Joanna" language="pl-PL">
-        Dzień dobry! Tu wirtualna recepcja. W czym mogę pomóc?
-      </Say>
+      <Say>Hello from AI</Say>
     </Response>
   `);
 });
@@ -20,6 +18,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server działa na porcie " + PORT);
-});
+app.listen(PORT);
